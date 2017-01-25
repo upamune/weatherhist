8bb456aa0e4c695db8f1f5e2ee49b9fcbfeb3354
upamune <jajkeqos@gmail.com>
2017-01-25 21:00:28 +0900

    add base structs

--- daily.go
+++ daily.go
@@ -1,1 +1,64 @@
 package weatherhist
+
+import "time"
+
+const DailyPath = "daily_a1.php"
+
+type Daily struct {
+	Days []Day
+}
+
+type Day struct {
+	Date            time.Time
+	Precipitation Precipitation
+	Temperature Temperature
+	Aerovane Aerovane
+	HoursOfSunlight float32
+	Snow Snow
+}
+
+type Precipitation struct {
+	Total            float32
+	MaxPrecipitation MaxPrecipitation
+}
+
+type MaxPrecipitation struct {
+	Hourly          float32
+	EveryTenMinutes float32
+}
+
+type Temperature struct {
+	Average float32
+	Highest float32
+	Lowest  float32
+}
+
+type Aerovane struct {
+	AverageWindSpeed          float32
+	MaxWindSpeed MaxWindSpeed
+	MaxInstantaneousSpeed MaxInstantaneousSpeed
+	MostFrequentWindDirection string
+}
+
+type MaxWindSpeed struct {
+	Speed     float32
+	Direction string
+}
+
+type MaxInstantaneousSpeed struct {
+	Speed     float32
+	Direction string
+}
+
+type Snow struct {
+	SnowFall    SnowFall
+	DeepestSnow DeepestSnow
+}
+
+type SnowFall struct {
+	Total float32
+}
+
+type DeepestSnow struct {
+	Value float32
+}
